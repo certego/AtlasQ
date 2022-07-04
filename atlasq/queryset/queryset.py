7104810fc9bc41a01ac456b720e176653f059e1f
Simone Berni <simone.berni2@studio.unibo.it>
2022-07-04 12:15:08 +0200

    Fix retrieval (#8)
    * Fixing
    
    * Fix

--- atlasq/queryset/queryset.py
+++ atlasq/queryset/queryset.py
@@ -53,8 +53,8 @@ class AtlasQuerySet(QuerySet):
 
     @property
     def _cursor(self):
-        self._search_result = self.aggregate(self._aggrs)
-        logger.debug(self._search_result)
+        if not self._search_result:
+            self._search_result = self.aggregate(self._aggrs)
         return super()._cursor
 
     @property
@@ -64,7 +64,7 @@ class AtlasQuerySet(QuerySet):
         # unfortunately here we have to actually run the query to get the objects
         # i do not see other way to do this atm
         self._query_obj = Q(id__in=[obj["_id"] for obj in self._search_result if obj])
-        logger.debug(self._query_obj)
+        logger.debug(self._query_obj.to_query(self._document))
         return super()._query
 
     def __call__(self, q_obj=None, **query):

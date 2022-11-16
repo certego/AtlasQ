83bfdb2d79f817006af73d6be94c1fd03f22f1fa
0ssigeno <simone.berni2@studio.unibo.it>
2022-11-16 11:38:40 +0100

    Added test to cover case

--- tests/queryset/test_transform.py
+++ tests/queryset/test_transform.py
@@ -13,6 +13,27 @@ from tests.test_base import TestBaseCase
 
 
 class TestTransformSteps(TestBaseCase):
+    def test_merge_embedded_documents_to_not_embedded(self):
+        list_of_objs = [
+            {
+                "text": {
+                    "query": "aaa",
+                    "path": "field.field2.field3",
+                }
+            }
+        ]
+        obj = {
+            "embeddedDocument": {
+                "path": "field",
+                "operator": {
+                    "compound": {
+                        "must": [{"text": {"query": "bbb", "path": "field.field4"}}]
+                    }
+                },
+            }
+        }
+        AtlasTransform.merge_embedded_documents(obj, list_of_objs)
+
     def test_merge_embedded_documents_multi_level_different_level(self):
         obj = {
             "embeddedDocument": {

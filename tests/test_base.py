808bca9637bc65a10d38da9b4f64afd5cbbbabd8
0ssigeno <s.berni@certego.net>
2023-05-29 10:30:30 +0200

    Fix db_name

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,9 +5,12 @@ from mongoengine import connect, disconnect
 
 
 class TestBaseCase(TestCase):
+
+    db_name = "mongoenginetest"
+
     @classmethod
     def setUpClass(cls) -> None:
-        connect(db="mongoenginetest", mongo_client_class=mongomock.MongoClient)
+        connect(db=cls.db_name, mongo_client_class=mongomock.MongoClient)
 
     @classmethod
     def tearDownClass(cls) -> None:
